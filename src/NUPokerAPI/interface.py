7201fcd19d3146c78cc1a9d9b04c80e9eafa0db2
Gabe <gabriel.reece.perry@outlook.com>
2026-09-17 22:34:20 +0100

    Switched fully to websockets and implemented matchmaking

--- src/NUPokerAPI/interface.py
+++ src/NUPokerAPI/interface.py
@@ -40,14 +40,13 @@ def card_to_string(card:int = -1):
     return f"{card_strings[card % 13]} of {suit_strings[card // 13]}"
 
 class PokerInterface:
-    def __init__(self, bot_name: str, passcode: str, address: str):
+    def __init__(self, api_key: str, address: str):
 
         if address[:5] != "ws://":
             raise Exception("Invalid address, please define ws:// preceding server URI")
         
         self.address = address
-        self.bot_name = bot_name
-        self.passcode = passcode
+        self.api_key = api_key
 
         try:
             self.s = connect(address)
@@ -61,11 +60,12 @@ class PokerInterface:
 
     def login(self):
         # Attempt server sign-in
-        data = json.dumps({"username": self.bot_name, "password": self.passcode})
+        data = json.dumps({"type": "login", "api_key": self.api_key})
         try:
             self.s.send(data)
             response = json.loads(self.s.recv())
-            if response['login']:
+            if response['success']:
+                logger.info(f'{response['info']}')
                 return True
             else:
                 logger.error(f'Could not log into poker server: {response['info']}')
@@ -74,53 +74,24 @@ class PokerInterface:
             logger.error(f"Could not log into poker server:\n{e}")
 
     def enter_matchmaking(self, timeout: int = 30):
-        # Tries to enter matchmaking queue, if successful, query for confirmation of game entry until timeout.
+        data = json.dumps({"type": "enter_matchmaking"})
         try:
-            if not self._matchmaking_request():
-                raise Exception(f"Could not enter matchmaking queue.")
-            logger.info("Queued for match.")
-            for i in range(0, timeout):
-                queued, match = self.check_matchmaking()
-                if queued:
-                    logger.info(f"Match found! Match ID: {match}.")
-                    return match
-                sleep(1)
-            raise Exception(f"Timeout after {timeout} seconds.")
-        except Exception as e:
-            logger.error(e)
-            return None
-
-    def _matchmaking_request(self):
-        # Attempts to enter matchmaking queue, if successful, return True. Else, return False.
-        try:
-            queue_request = requests.Request('POST', f"{self.address}:{self.port}/enter_queue")
-            queue_request = self.s.prepare_request(queue_request)
-            r = self.s.send(queue_request, timeout=5)
-            if r.status_code == 200:
-                return True
-            return False
+            self.s.send(data)
+            response = json.loads(self.s.recv())
+            if response['success']:
+                # Entered matchmaking
+                logger.info(f'{response['info']}')
+                # Waits for confirmation that client is in game
+                response = json.loads(self.s.recv())
+                if response['success'] and response['status'] == 3:
+                    logger.info(f'{response['info']}')
+                    return True
+            else:
+                logger.error(f'Could not enter matchmaking: {response['info']}')
+                return False
         except Exception as e:
             logger.error(e)
             return False
-
-    def check_matchmaking(self):
-        # Checks to see whether match making has placed bot in match.
-        try:
-            check_request = requests.Request("GET", f"{self.address}:{self.port}/query_queue")
-            check_request.headers['Content-Type'] = 'application/json'
-            check_request = self.s.prepare_request(check_request)
-            r = self.s.send(check_request, timeout=5)
-            if r.status_code != 200:
-                raise Exception(f"Contacted server, could not check matchmaking. Error: {r.status_code}")
-            status = json.loads(r.text)
-            if status["queued"]:
-                return True, status["match"]
-            return False, 0
-        except Exception as e:
-            logger.error(e)
-            return None, None
-
-    def _ping(self):
-        if requests.get(f"{self.address}:{self.port}", timeout=5).status_code != 200:
-            return True
-        return False
+    
+    def close(self):
+        self.s.close()
